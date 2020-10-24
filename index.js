4f50a443e0f1c2af91d38967cded349fcf99871d
ElBoDeLaJusticia <guillecoe2015@gmail.com>
2020-10-23 22:29:00 -0300

    FUNCIONA
    Funciona medio ahi la funcion de play con el titulo de una cancion

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ client.on('guildMemberAdd', (member) => {});
 client.on('guildMemberRemove', (member) => {});
 
 //Funciones con mensajes
-client.on('message', (message) => {
+client.on('message', async (message) => {
 	//Evitar bucle infinito
 	if (message.author.bot) return;
 
@@ -62,91 +62,9 @@ client.on('message', (message) => {
 	const serverQueue = queue.get(message.guild.id);
 
 	// <-- CODIGO CMD PLAY (REPRODUCIR): -->
-
-	if(command === 'play') {
-		//verificamos que el usuario solicitante este conectado en un canal de voz.
-		const voiceChannel = message.member.voice.channel;
-		if (!voiceChannel) 
-			return message.channel.send('¡Necesitas unirte a un canal de voz para reproducir música!');
-		 
-		//verificamos que el bot tenga permisos de conectar y de hablar en el canal de voz.
-		const permissions = voiceChannel.permissionsFor(message.client.user);
-		if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
-			return message.channel.send('¡Necesito permisos para unirme y hablar en el canal de voz!');
-		}
-
-		// <-- Capturamos la información de la música a reproducir -->
-
-		var opts = {
-			maxResults: 1, //Maximo de resultados a encontrar
-			key: 'API-KEY-YOUTUBE', //Necesitas una CLAVE de la API de youtube. 
-			type: "video" // Que tipo de resultado a obtener.
-		};
-		
-		(async function(){
-			const songArg = await search(args.join(' '), opts);
-			const songURL = songArg.results[0].link;
-			const songInfo = await ytdl.getInfo(songURL);
-		})()
-
-		/*const songArg = await search(args.join(' '), opts);
-		const songURL = songArg.results[0].link;
-		const songInfo = await ytdl.getInfo(songURL);*/
-		 
-		const song = {
-			title: songInfo.title,
-			url: songInfo.video_url,
-			author: message.author.tag
-		   };
-		   
-		// <-- Verificamos la lista de canciones de un servidor -->
-
-		if (!serverQueue) {
-		// Si NO hay una lista de música.
-		// <-- Creamos nuestra cola de música a reproducir  -->
-			const queueObject = {
-				textChannel: message.channel, //guardamos el canal de texto
-				voiceChannel: voiceChannel, // guardamos el canal de voz
-				connection: null, // un objeto para la conexión 
-				songs: [], // creamos la lista de canciones
-				volume: 5, // volumen al iniciar la cola
-				playing: true, // un objeto para validar la cola de música en reproducción.
-		    	};
-		    	// <-- Establecer la cola de música  -->
-
-			// Creando el conjunto de datos para nuestra cola de música
-			queue.set(message.guild.id, queueObject);
-
-			// Agregamos las canciones al conjunto de datos
-			queueObject.songs.push(song);
-   		}else {
-		// Si HAY una lista de música reproduciendo.
-		serverQueue.songs.push(song);
-		console.log(serverQueue.songs);
-		return message.channel.send(`**${song.title}** ha sido añadido a la cola!, por: ${message.author.tag}`);
-		}
-
-		// <-- Conectar al canal de voz  -->
-
-		try {
-			// Aquí unimos el bot al canal de voz y guardar nuestra conexión en nuestro objeto.
-			var connection = await voiceChannel.join();
-			queueObject.connection = connection;
-
-			message.channel.send(`Reproduciendo ahora: **${song.title}**`);
-
-			// Llamar a la función de reproducción para comenzar una canción.
-			play(message.guild, queueObject.songs[0]);
-
-    		} catch (err) {
-
-		// Imprimir el mensaje de error si el bot no puede unirse al chat de voz
-		console.log(err);
-		queue.delete(message.guild.id);
-		return message.channel.send(err);
-
-		}
-
+	if (command === 'play') {
+		execute(message, serverQueue);
+		return;
 	}
 
 	//Lista de ayuda
@@ -191,9 +109,71 @@ client.on('error', (e) => console.error(e));
 client.on('warn', (e) => console.warn(e));
 client.on('debug', (e) => console.debug(e));
 
-//Buscar la cancion
-async function(){
+// Verificar
+async function execute(message, serverQueue) {
+	const args = message.content.split(' ');
 
+	//verificamos que el usuario solicitante este conectado en un canal de voz.
+	const voiceChannel = message.member.voice.channel;
+	if (!voiceChannel)
+		return message.channel.send(
+			'¡Necesitas unirte a un canal de voz para reproducir música!'
+		);
+
+	//verificamos que el bot tenga permisos de conectar y de hablar en el canal de voz.
+	const permissions = voiceChannel.permissionsFor(message.client.user);
+	if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+		return message.channel.send(
+			'¡Necesito permisos para unirme y hablar en el canal de voz!'
+		);
+	}
+
+	// <-- Capturamos la información de la música a reproducir -->
+
+	var opts = {
+		maxResults: 1, //Maximo de resultados a encontrar
+		key: config.apiyt, //Necesitas una CLAVE de la API de youtube.
+		type: 'video', // Que tipo de resultado a obtener.
+	};
+
+	const songArg = await search(args.join(' '), opts);
+	const songURL = songArg.results[0].link;
+	const songInfo = await ytdl.getInfo(songURL);
+
+	const song = {
+		title: songInfo.title,
+		url: songInfo.video_url,
+		author: message.author.tag,
+	};
+
+	// <-- Verificamos la lista de canciones de un servidor -->
+
+	// Creating the contract for our queue
+	const queueContruct = {
+		textChannel: message.channel,
+		voiceChannel: voiceChannel,
+		connection: null,
+		songs: [],
+		volume: 5,
+		playing: true,
+	};
+	// Setting the queue using our contract
+	queue.set(message.guild.id, queueContruct);
+	// Pushing the song to our songs array
+	queueContruct.songs.push(song);
+
+	try {
+		// Here we try to join the voicechat and save our connection into our object.
+		var connection = await voiceChannel.join();
+		queueContruct.connection = connection;
+		// Calling the play function to start a song
+		play(message.guild, queueContruct.songs[0]);
+	} catch (err) {
+		// Printing the error message if the bot fails to join the voicechat
+		console.log(err);
+		queue.delete(message.guild.id);
+		return message.channel.send(err);
+	}
 }
 
 //Funcion de reproducir
@@ -201,22 +181,25 @@ function play(guild, song) {
 	const serverQueue = queue.get(guild.id);
 	// verificamos que hay musica en nuestro objeto de lista
 	if (!song) {
-	 serverQueue.voiceChannel.leave(); // si no hay mas música en la cola, desconectamos nuestro bot
-	 queue.delete(guild.id);
-	 return;
+		serverQueue.voiceChannel.leave(); // si no hay mas música en la cola, desconectamos nuestro bot
+		queue.delete(guild.id);
+		return;
 	}
 	// <-- Reproducción usando play()  -->
 
-	const dispatcher = serverQueue.connection.play(ytdl(song.url)).on('finish', () => {
-	// Elimina la canción terminada de la cola.
-	serverQueue.songs.shift();
-		
-	// Llama a la función de reproducción nuevamente con la siguiente canción
-	play(guild, serverQueue.songs[0]);
-	})
-	.on('error', error => {
-		console.error(error);
-	});
+	const dispatcher = serverQueue.connection
+		.play(ytdl(song.url))
+		.on('finish', () => {
+			// Elimina la canción terminada de la cola.
+			serverQueue.songs.shift();
+
+			// Llama a la función de reproducción nuevamente con la siguiente canción
+			play(guild, serverQueue.songs[0]);
+		})
+		.on('error', (error) => {
+			console.error(error);
+		});
 	// Configuramos el volumen de la reproducción de la canción
- 	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+	serverQueue.textChannel.send(`Reproduciendo: **${song.title}**`);
 }
