e17bb02fe089d9061c2e3ac3720941fc000b3418
GuilleCoelho <guillecoelho@protonmail.com>
2022-01-28 11:21:35 -0300

    Probando agregar comando para agregar preguntas usando argumentos

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 
-//Para la reproduccion de musica
-const ytdl = require('ytdl-core');
-const search = require('youtube-search');
-const queue = new Map(); // Cola de reproduccion
-
 //Importo archivos necesarios
 let prefix = config.prefix;
 const filter = require('./filter.js');
@@ -45,11 +40,11 @@ client.on('message', async (message) => {
 	}
 
 	//Obtener los comandos de un mensaje
-	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const args = message.content.slice(prefix.length).trim().split(' ');
 	const command = args.shift().toLowerCase();
 
 	//Evitar bucles con prefijos
-	if (!message.content.startsWith(prefix)) return;
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	//-----> COMANDOS <------
 	//Prueba de log
@@ -57,14 +52,9 @@ client.on('message', async (message) => {
 		message.client.channels.cache.get(config.logChannel).send('Funca');
 	}
 
-	//Reproducir musica
-	// Esta constante 'serverQueue' nos permitira saber si un servidor tiene una lista de musica reproduciendo.
-	const serverQueue = queue.get(message.guild.id);
-
-	// <-- CODIGO CMD PLAY (REPRODUCIR): -->
-	if (command === 'play') {
-		execute(message, serverQueue);
-		return;
+	//Subir pregunta
+	if (command == 'pregnueva') {
+		message.channel.send(args);
 	}
 
 	//Lista de ayuda
@@ -74,14 +64,14 @@ client.on('message', async (message) => {
 			.setAuthor(message.author.username, message.author.displayAvatarURL())
 			.setColor('#f67766')
 			.setDescription(
-				'Aqui te dejo una serie de comandos que puedes utilizar conmigo'
+				'Aqui te dejo una serie de comandos que puedes utilizar conmigo:'
 			)
 			.setFooter('No dudes en llamarme otra vez', client.user.avatarURL())
 			.setImage(message.author.displayAvatarURL())
 			.setThumbnail(message.author.displayAvatarURL())
 			.setTimestamp()
-			.setURL('https://github.com/ElBoDeLaJusticia')
-			.addField('Comandos:', 'xd tengo que pensarlos');
+			.setURL('https://github.com/GuilleCoelho')
+			.addField('Comandos:', 'bo! pregnueva "argumento": agrega una pregunta');
 		message.channel.send({ embed: embedDatos });
 	}
 });
@@ -108,98 +98,3 @@ client.login(config.token);
 client.on('error', (e) => console.error(e));
 client.on('warn', (e) => console.warn(e));
 client.on('debug', (e) => console.debug(e));
-
-// Verificar
-async function execute(message, serverQueue) {
-	const args = message.content.split(' ');
-
-	//verificamos que el usuario solicitante este conectado en un canal de voz.
-	const voiceChannel = message.member.voice.channel;
-	if (!voiceChannel)
-		return message.channel.send(
-			'¡Necesitas unirte a un canal de voz para reproducir música!'
-		);
-
-	//verificamos que el bot tenga permisos de conectar y de hablar en el canal de voz.
-	const permissions = voiceChannel.permissionsFor(message.client.user);
-	if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
-		return message.channel.send(
-			'¡Necesito permisos para unirme y hablar en el canal de voz!'
-		);
-	}
-
-	// <-- Capturamos la información de la música a reproducir -->
-
-	var opts = {
-		maxResults: 1, //Maximo de resultados a encontrar
-		key: config.apiyt, //Necesitas una CLAVE de la API de youtube.
-		type: 'video', // Que tipo de resultado a obtener.
-	};
-
-	const songArg = await search(args.join(' '), opts);
-	const songURL = songArg.results[0].link;
-	const songInfo = await ytdl.getInfo(songURL);
-
-	const song = {
-		title: songInfo.title,
-		url: songInfo.video_url,
-		author: message.author.tag,
-	};
-
-	// <-- Verificamos la lista de canciones de un servidor -->
-
-	// Creating the contract for our queue
-	const queueContruct = {
-		textChannel: message.channel,
-		voiceChannel: voiceChannel,
-		connection: null,
-		songs: [],
-		volume: 5,
-		playing: true,
-	};
-	// Setting the queue using our contract
-	queue.set(message.guild.id, queueContruct);
-	// Pushing the song to our songs array
-	queueContruct.songs.push(song);
-
-	try {
-		// Here we try to join the voicechat and save our connection into our object.
-		var connection = await voiceChannel.join();
-		queueContruct.connection = connection;
-		// Calling the play function to start a song
-		play(message.guild, queueContruct.songs[0]);
-	} catch (err) {
-		// Printing the error message if the bot fails to join the voicechat
-		console.log(err);
-		queue.delete(message.guild.id);
-		return message.channel.send(err);
-	}
-}
-
-//Funcion de reproducir
-function play(guild, song) {
-	const serverQueue = queue.get(guild.id);
-	// verificamos que hay musica en nuestro objeto de lista
-	if (!song) {
-		serverQueue.voiceChannel.leave(); // si no hay mas música en la cola, desconectamos nuestro bot
-		queue.delete(guild.id);
-		return;
-	}
-	// <-- Reproducción usando play()  -->
-
-	const dispatcher = serverQueue.connection
-		.play(ytdl(song.url))
-		.on('finish', () => {
-			// Elimina la canción terminada de la cola.
-			serverQueue.songs.shift();
-
-			// Llama a la función de reproducción nuevamente con la siguiente canción
-			play(guild, serverQueue.songs[0]);
-		})
-		.on('error', (error) => {
-			console.error(error);
-		});
-	// Configuramos el volumen de la reproducción de la canción
-	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-	serverQueue.textChannel.send(`Reproduciendo: **${song.title}**`);
-}
